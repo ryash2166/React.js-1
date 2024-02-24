98ca4456162c0191264b996e85f88b932f455fa4
Yash <rakholiyayash10@gmail.com>
2024-02-24 18:36:31 +0530

    24/02/2023

--- Javascript/RegExp.js
+++ Javascript/RegExp.js
@@ -15,21 +15,21 @@
 /* Number */
 
 {
-    const regex2 = /(\d{3})\D(\d{3})(\d{2})(\d{2})/g;
-    const result3 = regex2.exec('My phone number is: 123 1231212.');
+    const regex2 = /(\d{3})\D(\d{3})\D(\d{2})\D(\d{2})/g;
+    const result3 = regex2.exec('My phone number is: 123 123 12 12.');
     console.log(result3);
    
 }
 
 {
-    const regex2 = /(\d{2})\D(\d{2})(\d{2})(\d{2})(\d{2})/g;
-    const result3 = regex2.exec('My phone number is: 12 12121212.');
+    const regex2 = /(\d{2})\D(\d{2})\D(\d{2})\D(\d{2})\D(\d{2})/g;
+    const result3 = regex2.exec('My phone number is: 12 12 12 12 12.');
     console.log(result3);
 }
 
 {
-    const regex2 = /(\d{4})\D(\d{4})(\d{2})/g;
-    const result3 = regex2.exec('My phone number is: 1234 123412.');
+    const regex2 = /(\d{4})\D(\d{4})\D(\d{2})/g;
+    const result3 = regex2.exec('My phone number is: 1234 1234 12.');
     console.log(result3);
 }
 
--- Javascript/object.js
+++ Javascript/object.js
@@ -112,28 +112,73 @@
         }
     }]
 
-    const input = prompt("Enter This Name Mobile, Tour, Treak, Bike, Car For Full List: ");
+
+    // const input = prompt("Enter This Name Mobile, Tour, Treak, Bike, Car For Full List: ");
     
-    if(input == "Mobile"){
-        console.log("Mobile",list[0]["Mobile"]);
-    }
-    else if(input == "Tour"){
-        console.log("Tour",list[0]["Tour"]);
+    // if(input == "Mobile"){
+    //     console.log("Mobile",list[0]["Mobile"]);
+    // }
+    // else if(input == "Tour"){
+    //     console.log("Tour",list[0]["Tour"]);
 
-    }
-    else if(input == "Treak"){
-        console.log("Treak",list[0]["Treak"]);
+    // }
+    // else if(input == "Treak"){
+    //     console.log("Treak",list[0]["Treak"]);
 
-    }
-    else if(input == "Bike"){
-        console.log("Bike:",list[0]["Bike"]);
+    // }
+    // else if(input == "Bike"){
+    //     console.log("Bike:",list[0]["Bike"]);
 
-    }
-    else if(input == "Car"){
-        console.log("Car:" ,list[0]["Car"]);
-    }
-    else{
-        console.log("Please! Refresh The Page and Enter Valid Input For Result.");
-    }
+    // }
+    // else if(input == "Car"){
+    //     console.log("Car:" ,list[0]["Car"]);
+    // }
+    // else{
+    //     console.log("Please! Refresh The Page and Enter Valid Input For Result.");
+    // }
 
+}
+
+{
+    let list1 = [
+        { No1: "Moto", price:"$229", category: "mobile" },
+        { No2: "Apple", price:"$449", category: "mobile" },
+        { No3: "Poco", price:"$109", category: "mobile" },
+        { No4: "Samsung", price:"$329", category: "mobile" },
+        { No5: "Pixel", price:"$389", category: "mobile" },
+
+        { Car1: "Chevelle", price:"$140000", category: "Car" },
+        { Car2: "Shelby", price:"$91450", category: "Car" },
+        { Car3: "Mustang", price:"$328900", category: "Car" },
+        { Car4: "Blazer", price:"$109945", category: "Car" },
+        { Car5: "Corvette", price:"$119500", category: "Car" },
+
+        { Fruit1: "Strawberry", price:"$23", category: "fruit" },
+        { Fruit2: "Brazil Nut", price:"$23", category: "fruit" },
+        { Fruit3: "Coconut", price:"$23", category: "fruit" },
+        { Fruit4: "Fig", price:"$38", category: "fruit" },
+        { Fruit5: "Persimmon", price:"$89", category: "fruit" },
+
+        { Tour1: "Rishikesh", price:"$39", category: "tour" },
+        { Tour2: "Ladak", price:"$38", category: "tour" },
+        { Tour3: "Spiti", price:"$89", category: "tour" },
+        { Tour4: "Goa", price:"$29", category: "tour" },
+        { Tour5: "Kerala", price:"$59", category: "tour" },
+
+        { Treak1: "Kedarkantha", price:"$87", category: "treak" },
+        { Treak2: "Brahmatal", price:"$56", category: "treak" },
+        { Treak3: "Tarsar", price:"$47", category: "treak" },
+        { Treak4: "Nandadevi", price:"$32", category: "treak" },
+        { Treak5: "Hampta", price:"$23", category: "treak" }
+    ]; 
+
+    let output = prompt("Enter This Name Mobile, Tour, Treak, Fruit, Car For Full List: ");
+    let output1=  list1.filter(list1 => list1.category == output);
+
+    console.log(output1);
+
+    // for (let i = 0; i < output.length; i++) {
+    //     console.log(output1[i])
+    // };
+    
 }
