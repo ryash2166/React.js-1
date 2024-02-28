61b4b13ec59225287a3f4da25104e4538040a01c
Yash <rakholiyayash10@gmail.com>
2024-02-28 16:04:14 +0530

    28/01/2024

--- Javascript/object.js
+++ Javascript/object.js
@@ -178,23 +178,10 @@
 
     // console.log(output1);
 
-    
-
-    function myFunction() {
-        var x = document.getElementById("myText").list1;
-
-        var y =  x;
-
-        console.log(y);
-
-        document.getElementById("demo").innerHTML = x;
-      }
-
-    
-
     // for (let i = 0; i < output.length; i++) {
     //     console.log(output1[i])
     // };
+    
     
 }
 
@@ -216,4 +203,47 @@
     //     bark:  "woof woof!"
     // }
     //     console.log(dog1.name + " say " + dog1.bark)
+}
+
+{
+    let list1 = [
+        { No1: "Moto", price:"$229", category: "mobile" },
+        { No2: "Apple", price:"$449", category: "mobile" },
+        { No3: "Poco", price:"$109", category: "mobile" },
+        { No4: "Samsung", price:"$329", category: "mobile" },
+        { No5: "Pixel", price:"$389", category: "mobile" },
+
+        { Car1: "Chevelle", price:"$140000", category: "car" },
+        { Car2: "Shelby", price:"$91450", category: "car" },
+        { Car3: "Mustang", price:"$328900", category: "car" },
+        { Car4: "Blazer", price:"$109945", category: "car" },
+        { Car5: "Corvette", price:"$119500", category: "car" },
+
+        { Fruit1: "Strawberry", price:"$23", category: "fruit" },
+        { Fruit2: "Brazil Nut", price:"$23", category: "fruit" },
+        { Fruit3: "Coconut", price:"$23", category: "fruit" },
+        { Fruit4: "Fig", price:"$38", category: "fruit" },
+        { Fruit5: "Persimmon", price:"$89", category: "fruit" },
+
+        { Tour1: "Rishikesh", price:"$39", category: "tour" },
+        { Tour2: "Ladak", price:"$38", category: "tour" },
+        { Tour3: "Spiti", price:"$89", category: "tour" },
+        { Tour4: "Goa", price:"$29", category: "tour" },
+        { Tour5: "Kerala", price:"$59", category: "tour" },
+
+        { Treak1: "Kedarkantha", price:"$87", category: "treak" },
+        { Treak2: "Brahmatal", price:"$56", category: "treak" },
+        { Treak3: "Tarsar", price:"$47", category: "treak" },
+        { Treak4: "Nandadevi", price:"$32", category: "treak" },
+        { Treak5: "Hampta", price:"$23", category: "treak" }
+    ]; 
+
+    function getVal() {
+        const val = document.querySelector('input').value;
+        let output1=  list1.filter(list1 => list1.category == val);
+
+        console.log(val);
+        console.log(output1);
+
+      }
 }
