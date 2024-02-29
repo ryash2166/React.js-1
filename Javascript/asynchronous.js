3f09b2f6c3748e66cfe758a98e7f4aec0d733940
Yash <rakholiyayash10@gmail.com>
2024-02-29 12:27:09 +0530

    29/02/2024

--- Javascript/asynchronous.js
+++ Javascript/asynchronous.js
@@ -1,8 +1,53 @@
-{
-    setInterval(
-        function () {
-            let date = new Date().toLocaleTimeString();
-            console.log(date);
-        },2000
-    )
+/* Javascript Asynchronous Method */
+
+/* SetTimeOut Method */
+
+//  setTimeout(function , delay , perameter1 , perameter2 , ...)
+
+// setTimeout(
+//     function () {
+        
+//     }, delaytime
+// )
+
+function print() {
+    console.log("Hello Beta");
 }
+
+setTimeout(print , 2000);
+
+setTimeout(
+    function jeet() {
+        console.log("Vah Kya Jeet Hai");
+    },3000
+)
+
+setTimeout(() => console.log("Ye to kahi dekhela lagta hai") , 4000);
+
+function win(name , match) {
+    console.log(`On this series ${name} win already ${match} Matches.`);
+}
+setTimeout(win , 5000 , "India" , 3)
+
+/* SetInterval */
+
+//  setInterval(function , delay , perameter1 , perameter2 , ...)
+
+function sayHello() {
+    console.log('Hello')
+  }
+  setInterval(sayHello, 2000)
+
+  function win(name , match) {
+      console.log(`On this series ${name} win already ${match} Matches.`);
+  }
+  setInterval(win , 5000 , "India" , 3);
+
+// {
+//     setInterval(
+//         function () {
+//             let date = new Date().toLocaleTimeString();
+//             console.log(date);
+//         },2000
+//     )
+// }
