8b199b8784264fcdb78731e7789d313e12cbc9ab
Yash <rakholiyayash10@gmail.com>
2024-02-29 12:27:38 +0530

    29/02/2024

--- Javascript/asynchronous.js
+++ Javascript/asynchronous.js
@@ -10,38 +10,38 @@
 //     }, delaytime
 // )
 
-function print() {
-    console.log("Hello Beta");
-}
+// function print() {
+//     console.log("Hello Beta");
+// }
 
-setTimeout(print , 2000);
+// setTimeout(print , 2000);
 
-setTimeout(
-    function jeet() {
-        console.log("Vah Kya Jeet Hai");
-    },3000
-)
+// setTimeout(
+//     function jeet() {
+//         console.log("Vah Kya Jeet Hai");
+//     },3000
+// )
 
-setTimeout(() => console.log("Ye to kahi dekhela lagta hai") , 4000);
+// setTimeout(() => console.log("Ye to kahi dekhela lagta hai") , 4000);
 
-function win(name , match) {
-    console.log(`On this series ${name} win already ${match} Matches.`);
-}
-setTimeout(win , 5000 , "India" , 3)
+// function win(name , match) {
+//     console.log(`On this series ${name} win already ${match} Matches.`);
+// }
+// setTimeout(win , 5000 , "India" , 3)
 
 /* SetInterval */
 
 //  setInterval(function , delay , perameter1 , perameter2 , ...)
 
-function sayHello() {
-    console.log('Hello')
-  }
-  setInterval(sayHello, 2000)
+// function sayHello() {
+//     console.log('Hello')
+//   }
+//   setInterval(sayHello, 2000)
 
-  function win(name , match) {
-      console.log(`On this series ${name} win already ${match} Matches.`);
-  }
-  setInterval(win , 5000 , "India" , 3);
+//   function win(name , match) {
+//       console.log(`On this series ${name} win already ${match} Matches.`);
+//   }
+//   setInterval(win , 5000 , "India" , 3);
 
 // {
 //     setInterval(
