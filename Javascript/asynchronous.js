a88b98b8318936ec50842f0d9d62b973b3e80b5c
Yash <rakholiyayash10@gmail.com>
2024-02-29 12:28:01 +0530

    29/02/2024

--- Javascript/asynchronous.js
+++ Javascript/asynchronous.js
@@ -43,11 +43,11 @@
 //   }
 //   setInterval(win , 5000 , "India" , 3);
 
-// {
-//     setInterval(
-//         function () {
-//             let date = new Date().toLocaleTimeString();
-//             console.log(date);
-//         },2000
-//     )
-// }
+{
+    setInterval(
+        function () {
+            let date = new Date().toLocaleTimeString();
+            console.log(date);
+        },2000
+    )
+}
