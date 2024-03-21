a31646b39ea6a6cdc26b5f124bd85caf0a988298
Yash <rakholiyayash10@gmail.com>
2024-03-21 10:22:14 +0530

    21/03/2024

--- Javascript/javaPro.js
+++ Javascript/javaPro.js
@@ -52,67 +52,67 @@
 
 /* 8. Email validation in JavaScript */
 
-{
+// {
 
-}
+// }
 
 /* 9. Letter Validation in JavaScript */
 
-{
+// {
 
-}
+// }
 
 /* 10. Number Validation in JavaScript */
 
-{
+// {
 
-}
+// }
 
 /* 11. Letter and Number Validation in JavaScript */
 
-{
+// {
 
-}
+// }
 
 /* 12. IP Address Validation in JavaScript */
 
-{
+// {
 
-}
+// }
 
 /* 13. Retrieve Today’s Date in JavaScript */
 
-{
+// {
     // let date = new Date().toLocaleDateString();
     // console.log(date);
-}
+// }
 
 /* 14. Reverse array javascript */
 
-{
-    // let num = [1 , 2 , 3 , 4 , 5]
-    // num.reverse ()
-    // console.log(num);
-}
+// {
+//     let num = [1 , 2 , 3 , 4 , 5]
+//     num.reverse ()
+//     console.log(num);
+// }
 
 /* 15. JavaScript concate or merge two Arrays */
 
-{
-    // let num1 = [1 , 2 , 3 , 4 , 5]
-    // let num2 = [6 , 7 , 8 , 9 , 10]
-    // let num3 = num1.concat(num2)
+// {
+//     let num1 = [1 , 2 , 3 , 4 , 5]
+//     let num2 = [6 , 7 , 8 , 9 , 10]
+//     let num3 = num1.concat(num2)
 
-    // console.log(num3);
-}
+//     console.log(num3);
+// }
 
 /* 16. Reverse String in JavaScript */
 
-{
+// {
     // let string = "Hello Reverse";
     // let reverse = string.split('').reverse().join('');
 
     // console.log(reverse);
-}
+// }
 
 /* 17. JavaScript Open a page using window.open() method */
 
