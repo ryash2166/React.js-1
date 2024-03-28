bfd89eeb6d1a5e8db417b9831d5e3ef8c3cde82f
Yash <rakholiyayash10@gmail.com>
2024-03-28 17:06:33 +0530

    28/03/2024

--- ReactJs/Yarn Install/hello/src/App.js
+++ ReactJs/Yarn Install/hello/src/App.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import './App.css';
 
 function App() {
