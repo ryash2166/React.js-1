c8f72e219e02c580d52da89d7532532c23eb9c19
Yash <rakholiyayash10@gmail.com>
2024-03-29 16:04:27 +0530

    29/03/2024

--- ReactJs/VIte Install/hello/src/App.jsx
+++ ReactJs/VIte Install/hello/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline bg-gradient-to-br from-yellow-200 to-orange-700">
       Hello world!
       </h1>
     </>
