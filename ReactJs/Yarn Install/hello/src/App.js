f0ca9210b7e5bfc53a1d352a6420685eae4e0d29
Yash <rakholiyayash10@gmail.com>
2024-03-29 15:15:46 +0530

    29/03/2024

--- ReactJs/Yarn Install/hello/src/App.js
+++ ReactJs/Yarn Install/hello/src/App.js
@@ -1,38 +1,15 @@
-// import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 
 function App() {
   return (
     <>
-        <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">TextUtils</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">About</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    {/* <Button variant="contained">Hello world</Button> */}
+      <Navbar title="TextUtils" />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below"/>
+      </div>
     </>
   );
 }
