7c4cc2d1fe5215000b7f68f11b7f445ffe155430
Yash <rakholiyayash10@gmail.com>
2024-04-02 13:18:26 +0530

    02/04/2024

--- ReactJs/Yarn Install/hello/src/App.js
+++ ReactJs/Yarn Install/hello/src/App.js
@@ -1,15 +1,46 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Alert from './Components/Alert';
 import TextForm from './Components/TextForm';
-
+// import About from './Components/About';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 2000);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled" , "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled" , "success"); 
+    }
+  }
   
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils"  mode={mode} toggleMode={toggleMode} />
+      <Alert alert = {alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below"/>
+        <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />
+        {/* <About/> */}
       </div>
     </>
   );
