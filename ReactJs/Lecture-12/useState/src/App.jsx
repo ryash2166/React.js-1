8cb9ac5f1bc051b39cfb879854ce34b4048febd7
ryash2166 <rakholiyayash10@gmail.com>
2024-04-11 18:49:15 +0530

    11/04/2024

---
+++ ReactJs/Lecture-12/useState/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Task1 from './Components/Task1'
+import Task2 from './Components/Task2'
+
+function App() {
+
+  return (
+    <>
+      <Task1 />
+      <Task2 />
+    </>
+  )
+}
+
+export default App
