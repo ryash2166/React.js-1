e22e5d90e2f2ea7b11875b9a336493067172b94d
ryash2166 <rakholiyayash10@gmail.com>
2024-04-11 10:24:10 +0530

    11/04/2024

---
+++ ReactJs/Install/VIte Install/hello/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import Button from 'react-bootstrap/Button';
+import Card from './Components/card'
+// import product from "../product.json"
+
+function App() {
+
+  return (
+    <>
+      <Card />
+    </>
+  )
+}
+
+export default App
