80964d4d7a0c3cc038282e54055e67cbcb7a1cda
ryash2166 <rakholiyayash10@gmail.com>
2024-04-29 18:52:47 +0530

    29/04/2024

---
+++ ReactJs/Core_Redux/JS-Redux/Redux.js
@@ -0,0 +1,85 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+/* constant */
+
+const BUY_ICECREAME = 'BUY_ICECREAME'
+const BUYICECREAME = 'BUYICECREAME'
+
+
+function buy_icecreame(){
+    return{
+        type:BUY_ICECREAME,
+        info:'First Redux Action'
+    }
+}
+
+function buyicecreame(){
+    return{
+        type:BUYICECREAME,
+        info:'Second Redux Action'
+    }
+}
+
+const initialState = {
+    numOfIcecreame : 20
+}
+
+const reducer  = (state = initialState , action) => {
+    switch(action.type){
+        case BUY_ICECREAME: return {...state , numOfIcecreame:state.numOfIcecreame - 1};
+        case BUYICECREAME: return {...state , numOfIcecreame:state.numOfIcecreame + 1};
+        default: return state
+    }
+}
+
+const store = createStore(reducer)
+
+console.log('Initial State' , store.getState());
+
+store.subscribe(() => console.log('Updated State' , store.getState()))
+
+store.dispatch(buy_icecreame())
+
+store.dispatch(buyicecreame())
+
+store.dispatch(buy_icecreame())
+
+
+
+
+const reducer1 = (state = 2, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + action.value;
+      case "DECREMENT":
+        return state - action.value;
+      case "MULTIPLICATION":
+        return state * action.value;
+      default:
+        return state;
+    }
+  };
+  
+  const store1 = createStore(reducer1);
+  
+  store1.subscribe(() => {
+    console.log("current state", store1.getState());
+  });
+  
+  store1.dispatch({
+    type: "MULTIPLICATION",
+    value: 5
+  });
+
+  store1.dispatch({
+    type: "INCREMENT",
+    value: 1
+  });
+  
+  
+  
+  store1.dispatch({
+    type: "DECREMENT",
+    value: 2
+  });
